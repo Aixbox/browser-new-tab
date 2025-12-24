442a9a876ff142ca58946a468ab2802a6fe5c58a
aixbox <1481051129@qq.com>
2025-12-24 16:46:45 +0800

    refactor: reorganize project structure and consolidate styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from 'sonner'
-import '@/app/globals.css'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
